cc5dfb92a6cdd99b6f275c55b0bd492e8d5f73a5
Ivan Bulychev <psmivan@gmail.com>
2024-05-30 15:14:30 +0300

    hw

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-const http = require('http');
+const http = require("http");
+const getUsers = require("./modules/users");
+const { URL } = require("url");
 
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, "http://127.0.0.1:3003");
+  const helloValue = url.searchParams.get("hello");
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  if (request.url === `/hello=Ivan`) {
+    response.status = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: aplication/json";
+    response.write(`hello, ${helloValue}`);
+    response.end();
+  }
 
+  
+  if (request.url === "/users") {
+    response.status = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: aplication/json";
+    response.write(getUsers());
+    response.end();
+
+    return;
+  }
+
+  if (request.url === "/") {
+    response.status = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: text/plain";
+    response.write("Hello, world");
+    response.end();
+    return;
+  }
+
+  response.status = 500;
+  response.statusMessage = "Ok";
+  response.header = "Content-Type: text/plain";
+  response.write("{}");
+  response.end();
+  // Написать обработчик запроса:
+  // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
+  // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
+  // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
+  // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
+  // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+});
+
+server.listen(3003, () => {
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 });
